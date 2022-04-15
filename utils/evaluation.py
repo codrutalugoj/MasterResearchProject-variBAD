163f3a965cc4195d68870a322d49a87631c7aafa
Jeremy Constantin Boerker <jcboerker@pm.me>
2022-04-15 11:14:12 +0200

    added option to run eval. outside of training regime

--- utils/evaluation.py
+++ utils/evaluation.py
@@ -20,7 +20,7 @@ def evaluate(args,
     if hasattr(args, 'test_env_name'):
         env_name = args.test_env_name
     if num_episodes is None:
-        num_episodes = args.max_rollouts_per_task
+        num_episodes = args.max_rollouts_per_task + args.extra_rollouts_per_task
     num_processes = args.num_processes
 
     # --- set up the things we want to log ---
@@ -43,7 +43,7 @@ def evaluate(args,
                          normalise_rew=args.norm_rew_for_policy,
                          ret_rms=ret_rms,
                          tasks=tasks,
-                         add_done_info=args.max_rollouts_per_task > 1,
+                         add_done_info=args.max_rollouts_per_task + args.extra_rollouts_per_task > 1,
                          )
     num_steps = envs._max_episode_steps
 
@@ -121,13 +121,14 @@ def visualise_behaviour(args,
                         compute_state_reconstruction_loss=None,
                         compute_kl_loss=None,
                         ):
+
     # initialise environment
     env = make_vec_envs(env_name=args.env_name,
                         seed=args.seed * 42 + iter_idx,
                         num_processes=1,
                         gamma=args.policy_gamma,
                         device=device,
-                        episodes_per_task=args.max_rollouts_per_task,
+                        episodes_per_task=args.max_rollouts_per_task + args.extra_rollouts_per_task,
                         normalise_rew=args.norm_rew_for_policy, ret_rms=ret_rms,
                         rank_offset=args.num_processes + 42,  # not sure if the temp folders would otherwise clash
                         tasks=tasks
