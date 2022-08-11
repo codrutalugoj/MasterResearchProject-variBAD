01145a3ab9b66a06f4dd09c0deb8767b4a9898b6
codrutalugoj <codrutalugoj@gmail.com>
2022-08-11 12:21:24 +0200

    gpu 2

--- models/meta_mu2.py
+++ models/meta_mu2.py
@@ -4,7 +4,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
+device = torch.device("cuda:2" if torch.cuda.is_available() else "cpu")
 
 class MetaMu2(nn.Module):
 
