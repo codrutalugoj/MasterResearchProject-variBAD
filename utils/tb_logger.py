KL=1 (for correct inference) + gpu 0

utils/tb_logger.py
@@ -5,7 +5,7 @@ import os
 import torch
 from torch.utils.tensorboard import SummaryWriter
 
-device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 
 class TBLogger: